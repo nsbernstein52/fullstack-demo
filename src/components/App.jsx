22d138c0fe74b9e4411c41eb3ec749e18ece244c
nsbernstein52 <57200889+nsbernstein52@users.noreply.github.com>
2020-03-04 12:38:13 -0700

    Create comment/placeholder for filter modfications

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,7 +25,8 @@ class App extends React.Component {
         <Nav
           filterHandler={this.filterHandler}
         />
-        {this.state.bugs.map((bug) => (
+        {/* {this.state.bugs.map((bug, BugTile.threatLevel = this.getState(filter)) => ( */}
+          {this.state.bugs.map((bug) => (
           <BugTile
             bugName={bug.bugName}
             bugDescription={bug.bugDescription}
@@ -35,10 +36,57 @@ class App extends React.Component {
             threatLevel={bug.threatLevel}
             key={bug.bugName}
           />
-        ))}
+        ), )}
       </table>
     );
   }
 }
 
 export default App;
+
+// original code
+
+// import React from 'react';
+// import Nav from './Nav.jsx';
+// import BugTile from './BugTile.jsx';
+// import exampleData from '../example-data/exampleData';
+
+// import '../styles/App.scss';
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       filter: 'None',
+//       bugs: exampleData,
+//     };
+//     this.filterHandler = this.filterHandler.bind(this);
+//   }
+
+//   filterHandler(filter) {
+//     this.setState({ filter });
+//   }
+
+//   render() {
+//     return (
+//       <table>
+//         <Nav
+//           filterHandler={this.filterHandler}
+//         />
+//         {this.state.bugs.map((bug) => (
+//           <BugTile
+//             bugName={bug.bugName}
+//             bugDescription={bug.bugDescription}
+//             reportedBy={bug.reportedBy}
+//             createdDate={bug.createdDate}
+//             assignedTo={bug.assignedTo}
+//             threatLevel={bug.threatLevel}
+//             key={bug.bugName}
+//           />
+//         ))}
+//       </table>
+//     );
+//   }
+// }
+
+// export default App;
